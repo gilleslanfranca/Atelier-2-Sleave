de7c65ff183b6878b621d2e8d2d529c6bd92e52e
Gilles Lanfranca <lanfranca@gmx.de>
2021-05-29 17:08:43 +0200

    Création des objets de connection

---
+++ dal/DataAccess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sleave.dal
+{
+    class DataAccess
+    {
+        /// <summary>
+        /// Chaine de connexion à la base de données
+        /// </summary>
+        private static string connectionString = "server=localhost;user id=responsable;password=pwd;database=mediatek86;SslMode=none";
+
+        /// <summary>
+        /// Controle si l'utillisateur a le droit de se connecter
+        /// </summary>
+        /// <param name="login"></param>
+        /// <param name="pwd"></param>
+        /// <returns></returns>
+    }
+}
