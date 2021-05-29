1366dcaccd6722fbe3ae7f8ed7a7edb9de2a57cb
Gilles Lanfranca <lanfranca@gmx.de>
2021-05-29 16:44:46 +0200

    Création de l'interface graphique des absences

---
+++ view/FrmAbsences.Designer.cs
@@ -0,0 +1,276 @@
+﻿
+namespace Sleave.view
+{
+    partial class FrmAbsences
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnValid = new System.Windows.Forms.Button();
+            this.cboAction = new System.Windows.Forms.ComboBox();
+            this.lblReason = new System.Windows.Forms.Label();
+            this.lblDateStart = new System.Windows.Forms.Label();
+            this.lblFirstName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblEnd = new System.Windows.Forms.Label();
+            this.cboDept = new System.Windows.Forms.ComboBox();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.txtDateStart = new System.Windows.Forms.TextBox();
+            this.txtDateEnd = new System.Windows.Forms.TextBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.dgvAbsence = new System.Windows.Forms.DataGridView();
+            this.dtpStart = new System.Windows.Forms.DateTimePicker();
+            this.dtpEnd = new System.Windows.Forms.DateTimePicker();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAbsence)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(701, 245);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 27;
+            this.btnCancel.Text = "Annuler";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnValid
+            // 
+            this.btnValid.Location = new System.Drawing.Point(601, 245);
+            this.btnValid.Name = "btnValid";
+            this.btnValid.Size = new System.Drawing.Size(75, 23);
+            this.btnValid.TabIndex = 26;
+            this.btnValid.Text = "Valider";
+            this.btnValid.UseVisualStyleBackColor = true;
+            // 
+            // cboAction
+            // 
+            this.cboAction.FormattingEnabled = true;
+            this.cboAction.Location = new System.Drawing.Point(601, 212);
+            this.cboAction.Name = "cboAction";
+            this.cboAction.Size = new System.Drawing.Size(175, 21);
+            this.cboAction.TabIndex = 25;
+            // 
+            // lblReason
+            // 
+            this.lblReason.AutoSize = true;
+            this.lblReason.Location = new System.Drawing.Point(522, 166);
+            this.lblReason.Name = "lblReason";
+            this.lblReason.Size = new System.Drawing.Size(30, 13);
+            this.lblReason.TabIndex = 24;
+            this.lblReason.Text = "Motif";
+            // 
+            // lblDateStart
+            // 
+            this.lblDateStart.AutoSize = true;
+            this.lblDateStart.Location = new System.Drawing.Point(522, 102);
+            this.lblDateStart.Name = "lblDateStart";
+            this.lblDateStart.Size = new System.Drawing.Size(60, 13);
+            this.lblDateStart.TabIndex = 23;
+            this.lblDateStart.Text = "Date début";
+            // 
+            // lblFirstName
+            // 
+            this.lblFirstName.AutoSize = true;
+            this.lblFirstName.Location = new System.Drawing.Point(522, 67);
+            this.lblFirstName.Name = "lblFirstName";
+            this.lblFirstName.Size = new System.Drawing.Size(43, 13);
+            this.lblFirstName.TabIndex = 22;
+            this.lblFirstName.Text = "Prénom";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(522, 35);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(29, 13);
+            this.lblLastName.TabIndex = 21;
+            this.lblLastName.Text = "Nom";
+            // 
+            // lblEnd
+            // 
+            this.lblEnd.AutoSize = true;
+            this.lblEnd.Location = new System.Drawing.Point(522, 134);
+            this.lblEnd.Name = "lblEnd";
+            this.lblEnd.Size = new System.Drawing.Size(44, 13);
+            this.lblEnd.TabIndex = 20;
+            this.lblEnd.Text = "Date fin";
+            // 
+            // cboDept
+            // 
+            this.cboDept.FormattingEnabled = true;
+            this.cboDept.Location = new System.Drawing.Point(601, 163);
+            this.cboDept.Name = "cboDept";
+            this.cboDept.Size = new System.Drawing.Size(175, 21);
+            this.cboDept.TabIndex = 19;
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(601, 64);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(175, 20);
+            this.txtFirstName.TabIndex = 18;
+            // 
+            // txtDateStart
+            // 
+            this.txtDateStart.Location = new System.Drawing.Point(601, 99);
+            this.txtDateStart.Name = "txtDateStart";
+            this.txtDateStart.Size = new System.Drawing.Size(175, 20);
+            this.txtDateStart.TabIndex = 17;
+            // 
+            // txtDateEnd
+            // 
+            this.txtDateEnd.Location = new System.Drawing.Point(601, 131);
+            this.txtDateEnd.Name = "txtDateEnd";
+            this.txtDateEnd.Size = new System.Drawing.Size(175, 20);
+            this.txtDateEnd.TabIndex = 16;
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Location = new System.Drawing.Point(601, 32);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(175, 20);
+            this.txtLastname.TabIndex = 15;
+            // 
+            // dgvAbsence
+            // 
+            this.dgvAbsence.AllowUserToAddRows = false;
+            this.dgvAbsence.AllowUserToDeleteRows = false;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.Black;
+            this.dgvAbsence.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.dgvAbsence.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dgvAbsence.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvAbsence.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.dgvAbsence.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(200)))), ((int)(((byte)(200)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvAbsence.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvAbsence.EnableHeadersVisualStyles = false;
+            this.dgvAbsence.Location = new System.Drawing.Point(13, 12);
+            this.dgvAbsence.MultiSelect = false;
+            this.dgvAbsence.Name = "dgvAbsence";
+            this.dgvAbsence.ReadOnly = true;
+            this.dgvAbsence.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvAbsence.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.dgvAbsence.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvAbsence.Size = new System.Drawing.Size(490, 412);
+            this.dgvAbsence.TabIndex = 28;
+            // 
+            // dtpStart
+            // 
+            this.dtpStart.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpStart.Location = new System.Drawing.Point(601, 99);
+            this.dtpStart.Name = "dtpStart";
+            this.dtpStart.Size = new System.Drawing.Size(175, 20);
+            this.dtpStart.TabIndex = 29;
+            // 
+            // dtpEnd
+            // 
+            this.dtpEnd.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpEnd.Location = new System.Drawing.Point(601, 131);
+            this.dtpEnd.Name = "dtpEnd";
+            this.dtpEnd.Size = new System.Drawing.Size(175, 20);
+            this.dtpEnd.TabIndex = 30;
+            // 
+            // FrmAbsences
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvAbsence);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnValid);
+            this.Controls.Add(this.cboAction);
+            this.Controls.Add(this.lblReason);
+            this.Controls.Add(this.lblDateStart);
+            this.Controls.Add(this.lblFirstName);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblEnd);
+            this.Controls.Add(this.cboDept);
+            this.Controls.Add(this.txtFirstName);
+            this.Controls.Add(this.txtDateStart);
+            this.Controls.Add(this.txtDateEnd);
+            this.Controls.Add(this.txtLastname);
+            this.Controls.Add(this.dtpEnd);
+            this.Controls.Add(this.dtpStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmAbsences";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Gestion des absences";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvAbsence)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnValid;
+        private System.Windows.Forms.ComboBox cboAction;
+        private System.Windows.Forms.Label lblReason;
+        private System.Windows.Forms.Label lblDateStart;
+        private System.Windows.Forms.Label lblFirstName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblEnd;
+        private System.Windows.Forms.ComboBox cboDept;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.TextBox txtDateStart;
+        private System.Windows.Forms.TextBox txtDateEnd;
+        private System.Windows.Forms.TextBox txtLastname;
+        private System.Windows.Forms.DataGridView dgvAbsence;
+        private System.Windows.Forms.DateTimePicker dtpStart;
+        private System.Windows.Forms.DateTimePicker dtpEnd;
+    }
+}
