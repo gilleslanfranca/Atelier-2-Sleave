06af5ddf709da4eaaa0e16f01c01e36c4a88a0b4
Gilles Lanfranca <lanfranca@gmx.de>
2021-05-29 18:46:41 +0200

    Cas d'utilisation n°1 - Se connecter

---
+++ control/Controller.cs
@@ -0,0 +1,53 @@
+﻿using Sleave.dal;
+using Sleave.view;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sleave.control
+{
+    /// <summary>
+    /// Classe de controle de l'application
+    /// </summary>
+    public class Controller
+    {
+        /// <summary>
+        /// Interface graphique de connexion
+        /// </summary>
+        private FrmConnection frmConnection;
+        
+        /// <summary>
+        /// Constructeur du controle : Initialise l'interface de connexion
+        /// </summary>
+        public Controller()
+        {
+            frmConnection = new FrmConnection(this);
+            frmConnection.ShowDialog();
+
+        }
+
+        /// <summary>
+        /// Demande une connextion avec les données saisies
+        /// Ouverture de l'interface Gestion du Personnel si correct
+        /// </summary>
+        /// <param name="login"></param>
+        /// <param name="pwd"></param>
+        /// <returns>Vrai ou Faux</returns>
+        public Boolean ControlAccess(string login, string pwd)
+        {
+            if (DataAccess.ControlAccess(login, pwd))
+            {
+                frmConnection.Hide();
+                FrmPersonnel frmPersonnel = new FrmPersonnel(this);
+                frmPersonnel.ShowDialog();
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- dal/DataAccess.cs
+++ dal/DataAccess.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sleave.connection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,5 +13,33 @@ namespace Sleave.dal
         /// Chaine de connexion à la base de données
         /// </summary>
         private static string connectionString = "server=localhost;user id=responsable;password=pwd;database=mediatek86;SslMode=none";
+
+        /// <summary>
+        /// Controle si l'utilisateur a le droit de se connecter
+        /// </summary>
+        /// <param name="login"></param>
+        /// <param name="pwd"></param>
+        /// <returns></returns>
+        public static Boolean ControlAccess(string login, string pwd)
+        {
+            string req = "select * from responsable res ";
+            req += "where res.login=@login and pwd=SHA2(@pwd, 256);";
+            Dictionary<string, object> parameters = new Dictionary<string, object>();
+            parameters.Add("@login", login);
+            parameters.Add("@pwd", pwd);
+            ConnectionDataBase curs = ConnectionDataBase.GetInstance(connectionString);
+            curs.ReqSelect(req, parameters);
+            if (curs.Read())
+            {
+                curs.Close();
+                return true;
+            }
+            else
+            {
+                curs.Close();
+                return false;
+            }
+        }
+
     }
 }
